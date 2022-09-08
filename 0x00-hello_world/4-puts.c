b3114f6d60306c15a7082967a77c48d7926cf5f8
tobiloba6 <owoyeyetobiloba@yahoo.com>
2022-09-08 15:55:45 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
