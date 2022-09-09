c92e1bcfcb56ea308117f494cb862e4e8353ac11
tobiloba6 <owoyeyetobiloba@yahoo.com>
2022-09-08 17:35:23 -0700

    latest commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - prints out statement followed by a new line, to the standard error.
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a new line, to the standard error.
  *
  * Return: 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1)
 }
