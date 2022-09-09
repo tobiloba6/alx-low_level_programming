bf1391a6c315f487ce6b380a569194403ee1f8f4
tobiloba6 <owoyeyetobiloba@yahoo.com>
2022-09-09 14:07:51 -0700

    corrections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,10 +22,8 @@ int main(void)
 		if (n < 0){	
 		printf("%i is negative\n", n);
 		}
-	}
 		else 	{
-		if (n == 0)
-		{printf("%i is zero\n", n);
+		printf("%i is zero\n", n);
 		}
 	}
 	return (0);
