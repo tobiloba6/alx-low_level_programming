5a0cac3d28fd6b1208521ebd976cad535eea95d2
tobiloba6 <owoyeyetobiloba@yahoo.com>
2022-09-09 17:10:30 -0700

    c variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
