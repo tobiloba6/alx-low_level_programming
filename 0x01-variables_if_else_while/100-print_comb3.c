222c18d9726880416640f5390b9b507022eae3dc
tobiloba6 <owoyeyetobiloba@yahoo.com>
2022-09-09 17:56:12 -0700

    task 13

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i,j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+
+				putchar(j + '0');
+
+				if (i != 8)
+				{
+					putchar(',');
+
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
