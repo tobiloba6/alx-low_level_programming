cbe488e84c449bd72e539841b3517fec0ed7583f
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-14 18:05:32 +0100

    corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * times_table - prints the 9 times table, starting with 0
- * 
+ *
  * Return: Always success
  */
 void times_table(void)
@@ -22,7 +22,7 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 0);
+				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) + '0');
 		}
