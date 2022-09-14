8eae518a041b727ecfc87ecef046c26b83efaae4
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-14 10:35:04 +0100

    c functions and nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,21 +12,21 @@ int print_sign(int n)
 	if (n > 0)
 
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
 
 	else if (n == 1)
 	{
 
-		_putchar("0");
+		_putchar('0');
 		return (0);
 	}
 
 	else
 	{
 
-		_putchar(*_*);
+		_putchar('_');
 		return (-1);
 	}
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * rw = row, col = column, d = digits
+ * Return: Always success
+ */
+void times_table(void)
+{
+
+	int rw, col, d;
+	for (rw = 0; rw <= 9; rw++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+	for (col =1; col <= 9; col++)
+	{
+		d = (rw * col);
+
+	if ((d / 10) > 0)
+	{
+
+		_putchar((d / 10) + '0');
+	}
+
+	else
+	{
+		_putchar(' ');
+	}
+	}
+	}
+}
+
