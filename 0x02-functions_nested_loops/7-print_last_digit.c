50cf5081fec3d3a01eac725555010fd44f2b316f
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-14 09:35:52 +0100

    jack bauer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,17 @@
  */
 int print_last_digit(int n)
 {
-	
+
 	int i;
-	i = -(n % 10);
-	
+	i = (n % 10);
+
+	if (i < 0)
+	{
+
+		i = (-1 * i);
+	}
+
 	_putchar(i + '0');
-	return (0);
+
+	return (i);
 }
