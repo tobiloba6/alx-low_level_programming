e49638648e167aba2b45cb135e838f4896fe1b3e
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-14 01:34:21 +0100

    print alphabet x10)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+
+	char c;
+	for (c = 'a'; n <= 'z'; c++)
+	{
+		char c = 0;
+		while (c < 10)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+		c++
+	}
+
