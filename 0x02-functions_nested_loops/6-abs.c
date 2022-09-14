ca745e2604aa83128fc5dbfa2e62fbb36056c467
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-14 08:53:11 +0100

    corrections

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
  * _abs - computes absolute value of an interger
- *
+ * @r: input letter
  * Return: Always sucess
  */
 int _abs(int r)
 {
-
-	int r;
 
 	return (r * ((r > 0) - (r < 0)));
 }
