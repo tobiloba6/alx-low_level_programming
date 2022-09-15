59f91eb3d1db1d7a634ff661515c878cb96a9325
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-15 05:26:53 +0100

    adavanced

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+			break;
+
+		if (((sum % 2) == 0)
+				total_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+		}
+		printf("%.Of\n", total_sum);
+
+		return (0);
+		}
+
