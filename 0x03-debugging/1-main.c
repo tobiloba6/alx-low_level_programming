03b30913ed41a8ddfe92d80bdaa04cd2c1486d3a
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-15 13:31:23 +0100

    corrections

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,24 +2,22 @@
 
 /**
  * main - causes an infinite loop
- * Return: 0
+ * Return: o
  */
 
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
 	/*
-	 * while (i < 10)
+	 * while (i < 0)
 	 * {
 	 * putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
