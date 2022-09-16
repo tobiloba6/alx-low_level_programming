ae9c52ba01fc950e8a96130a45c5802d03c1cdcd
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-16 19:58:14 +0100

    advanced tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - writes a function that prints an integer
+ * @n: integer to be printed
+ * Return: no return
+ */
+
+void print_number(int n)
+{
+	unsigned int a;
+
+	a = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		a = -n;
+	}
+	if (a / 10)
+		print_number(a / 10);
+	_putchar((a % 10) + '()')
+}
+
