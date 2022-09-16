b714b2146316d3f4469d3db380cc1f1b7c8e52b4
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-16 15:54:31 +0100

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 for success, 0 if otherwise
+ */
+
+int _isupper(int c);
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+
+	return (0);
+}
