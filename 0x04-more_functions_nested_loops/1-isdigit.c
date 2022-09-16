3814a54f06521494577c57c09ffcebac5481f7d4
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-16 16:08:21 +0100

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: character to be tested
+ * Return: 1 if success, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+
+	return (0);
+}
