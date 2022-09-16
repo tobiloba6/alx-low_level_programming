df7cd900c076374d9cccfef28a4c585c09d52d96
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-16 16:26:51 +0100

    3 print numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -8,7 +8,7 @@
  * Return: result of multiplication
  */
 
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	return (a * b);
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: 0 (always)
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
