ea01c8e0e6871381ea1ce8494339fcb3d34baf7f
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-22 17:59:38 +0100

    correction

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,14 +8,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
+        int tmp, index;
 
-	for (index = n - 1; index > n / 2; index--)
-	{
-		tmp = a[n - 1 - index];
+        for (index = n - 1; index > n / 2; index--)
+        {
+                tmp = a[n - 1 - index];
 
-		a[n - 1 - index] = a[index];
+                a[n - 1 - index] = a[index];
 
-		a[index] = tmp;
-	}
+                a[index] = tmp;
+        }
 }
+
