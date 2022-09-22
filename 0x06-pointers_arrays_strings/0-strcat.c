2a0ac8b79ceca762dbd2a9a73b635c819fd06b4b
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-22 10:21:28 +0100

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,18 +5,19 @@
  * @src: string to append to dest
  * @dest: string to be appended to
  *
- * Return: dest
+ * Return: returns to @dest
  */
 
 char *_strcat(char *dest, char *src)
 {
+
 	int i = 0, j = 0;
 
-	while (dest[i])
+	while (dest[j++])
 		i++;
 
 	for (j = 0; src[j]; j++)
 		dest[i] = src[j];
 
-	Return (dest);
+	return (dest);
 }
