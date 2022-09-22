fde728a5f2162572a6849323e949835781c4e185
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-22 17:13:01 +0100

    correction

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
+ *string_toupper - changes all lowercase letters of a string to uppercase
  * @str: string to be changed
  *
  * Return: string address
