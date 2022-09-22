98830e6a46b951ec9e9775e4d2e1152de1790ba9
Oluwatobi Owoyeye <owoyeyetobiloba@yahoo.com>
2022-09-22 17:09:09 +0100

    task 5

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *  reverse_array - reverses the content of an array of integers
- *  @a: array of integers 
- *  @n: no of elements
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers 
+ * @n: no of elements
  */
 
 void reverse_array(int *a, int n)
 {
 	int tmp, index;
+
 	for (index = n - 1; index > n / 2; index--)
 	{
 		tmp = a[n - 1 - index];
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string to be changed
+ *
+ * Return: string address
+ */
+
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+
